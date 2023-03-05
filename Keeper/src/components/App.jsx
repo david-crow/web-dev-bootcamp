2a81557ec515c6c84f973ac575c2f6e296f82d14
David Crow <davidrheacrow@gmail.com>
2023-03-05 18:26:04 -0500

    Update Keeper to add/delete notes

--- Keeper/src/components/App.jsx
+++ Keeper/src/components/App.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes.js";
+import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(note) {
+        setNotes([...notes, note]);
+    }
+
+    function deleteNote(id) {
+        setNotes((prev) => prev.filter((note, index) => index !== id));
+    }
+
     return (
         <div>
             <Header />
-            {notes.map((note) => (
+            <CreateArea onAdd={addNote} />
+            {notes.map((note, index) => (
                 <Note
-                    key={note.key}
+                    key={index}
+                    id={index}
                     title={note.title}
                     content={note.content}
+                    onDelete={deleteNote}
                 />
             ))}
             <Footer />
